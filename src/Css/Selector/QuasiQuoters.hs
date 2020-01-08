0c7499c3cba7127ed791eefb0572d720363faf3a
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-01-08 23:00:26 +0100

    start working on pattern construction

--- src/Css/Selector/QuasiQuoters.hs
+++ src/Css/Selector/QuasiQuoters.hs
@@ -17,7 +17,7 @@ import Data.Data(Data, cast)
 import Data.Text(pack, unpack)
 
 import Language.Haskell.TH.Quote(QuasiQuoter(QuasiQuoter, quoteExp, quotePat, quoteType, quoteDec))
-import Language.Haskell.TH.Syntax(Exp(AppE, VarE), Q, dataToExpQ, lift)
+import Language.Haskell.TH.Syntax(Exp(AppE, VarE), Pat, Q, dataToExpQ, dataToPatQ, lift)
 
 import Css.Selector.Core(SelectorGroup)
 import Css.Selector.Lexer(alexScanTokens)
@@ -31,6 +31,9 @@ parseCss = cssselector . alexScanTokens . filter ('\r' /=)
 liftDataWithText :: Data a => a -> Q Exp
 liftDataWithText = dataToExpQ ((((AppE (VarE 'pack) <$>) . lift . unpack) <$>) . cast)
 
+liftDataWithTextPat :: Data a => a -> Q Pat
+liftDataWithTextPat = dataToPatQ (const Nothing)
+
 -- | A quasiquoter that can be used to construct a 'SelectorGroup' for the given
 -- css selector. In case the css selector is invalid. A compiler error will be
 -- thrown (at compile time).
@@ -38,7 +41,7 @@ csssel :: QuasiQuoter
 csssel = QuasiQuoter {
     quoteExp = liftDataWithText . parseCss,
     -- TODO: maybe later generate pattern to match CSS selectors
-    quotePat = error "This quasiquoter does not generate patterns.",
+    quotePat = liftDataWithTextPat . parseCss,
     quoteType = error "This quasiquoter does not generate a type.",
     quoteDec = error "This quasiquoter does not generate declarations."
   }
