0a8c95354afd1fe135debe2df483d6b805511053
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-01-19 17:28:38 +0100

    make parsing of identifiers easier

--- src/Css/Selector/Utils.hs
+++ src/Css/Selector/Utils.hs
@@ -31,14 +31,16 @@ _initLast (a:as) = Just (go as a)
 -- string literal.
 readCssString :: String  -- ^ The string that contains the string literal in the css selector.
     -> String -- ^ A string that contains the content of the string literal.
-readCssString ('"':xs) = _readCssString '"' xs
-readCssString ('\'':xs) = _readCssString '\'' xs
-readCssString _ = error "The string should start with an \" or '."
+readCssString (c:xs) | elem c "'\"" = f
+    where f | Just (vs, c') <- _initLast xs = g c' vs
+            | otherwise = "The string literal should contain at least two quotation marks."
+              where  g c' vs | c == c' = _readCssString c vs
+                             | otherwise = "The start and end quotation mark should be the same."
+readCssString _ = error "The string should start with an \" or ' and end with the same quotation."
 
 _readCssString :: Char -> String -> String
 _readCssString c' = go
-    where go "" = error "The string should end with " ++ c' : "."
-          go [c] | c' == c = ""
+    where go [] = []
           go ('\\':'\n':xs) = go xs
           go ('\\':ca@(c:xs)) | c == c' = c : go xs
                               | otherwise = let ~(y,ys) = _parseEscape ca in y : go ys
@@ -48,7 +50,7 @@ _readCssString c' = go
 -- | Parse a given css identifier to the content of the identifier.
 readIdentifier :: String -- ^ The given css identifier to parse.
     -> String -- ^ The result of the parsing: the content of the identifier.
-readIdentifier = id
+readIdentifier = _readCssString '\\'
 
 _notEncode :: Char -> Bool
 _notEncode '\\' = False
