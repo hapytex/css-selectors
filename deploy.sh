0f1b74a0460197d1134a729b1877d7d6844b0ae7
Willem Van Onsem <vanonsem.willem@gmail.com>
2020-03-25 13:53:46 +0100

    from now on deployments are made on tags

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 stack sdist
 pth=$(stack path --dist-dir)
 
-/opt/cabal/bin/cabal upload -u "$1" -p "$2" "$pth"/*.tar.gz
+/opt/cabal/bin/cabal upload -u "$1" -p "$2" "$pth"/*.tar.gz --publish
 
 rm stack.yaml.lock
