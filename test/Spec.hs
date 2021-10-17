b1f331b2f7e53ee3a3ec9d1e25d4272ca0423614
Willem Van Onsem <vanonsem.willem@gmail.com>
2021-10-17 18:06:24 +0200

    update tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -22,82 +22,86 @@ tests = [
         testProperty "Encode-decode identifier" encodeDecodeId
     ],
     testGroup "Arbitrary css parsing" [
-        testProperty "Encode-decode css identity" encodeDecodeCss,
-        testProperty "Encode-decode css identity: selector group" (encodeDecodeCss' @SelectorGroup),
-        testProperty "Encode-decode css identity: selector" (encodeDecodeCss' @Selector),
-        testProperty "Encode-decode css identity: selector sequence" (encodeDecodeCss' @SelectorSequence),
-        testProperty "Encode-decode css identity: selector filter" (encodeDecodeCss' @SelectorFilter),
-        testProperty "Encode-decode css identity: namespace" (encodeDecodeCss' @Namespace),
-        testProperty "Encode-decode css identity: element name" (encodeDecodeCss' @ElementName),
-        testProperty "Encode-decode css identity: type selector" (encodeDecodeCss' @TypeSelector),
-        testProperty "Encode-decode css identity: attribute" (encodeDecodeCss' @Attrib),
-        testProperty "Encode-decode css identity: attribute name" (encodeDecodeCss' @AttributeName),
-        testProperty "Encode-decode css identity: class" (encodeDecodeCss' @Class),
-        testProperty "Encode-decode css identity: pseudo class" (encodeDecodeCss' @PseudoClass),
-        testProperty "Encode-decode css identity: pseudo element" (encodeDecodeCss' @PseudoElement),
-        testProperty "Encode-decode css identity: pseudo selector sequence" (encodeDecodeCss' @PseudoSelectorSequence)
+        testProperty "Encode-decode css identity" encodeDecodeCss
+      , testProperty "Encode-decode css identity: selector group" (encodeDecodeCss' @SelectorGroup)
+      , testProperty "Encode-decode css identity: selector" (encodeDecodeCss' @Selector)
+      , testProperty "Encode-decode css identity: selector sequence" (encodeDecodeCss' @SelectorSequence)
+      , testProperty "Encode-decode css identity: selector filter" (encodeDecodeCss' @SelectorFilter)
+      , testProperty "Encode-decode css identity: namespace" (encodeDecodeCss' @Namespace)
+      , testProperty "Encode-decode css identity: element name" (encodeDecodeCss' @ElementName)
+      , testProperty "Encode-decode css identity: type selector" (encodeDecodeCss' @TypeSelector)
+      , testProperty "Encode-decode css identity: attribute" (encodeDecodeCss' @Attrib)
+      , testProperty "Encode-decode css identity: attribute name" (encodeDecodeCss' @AttributeName)
+      , testProperty "Encode-decode css identity: class" (encodeDecodeCss' @Class)
+      , testProperty "Encode-decode css identity: pseudo class" (encodeDecodeCss' @PseudoClass)
+      , testProperty "Encode-decode css identity: pseudo element" (encodeDecodeCss' @PseudoElement)
+      , testProperty "Encode-decode css identity: pseudo selector sequence" (encodeDecodeCss' @PseudoSelectorSequence)
+      , testProperty "Encode-decode css identity: negation" (encodeDecodeCss' @Negation)
     ],
     testGroup "SelectorSequences" [
         testProperty "Adding and removing filters" addRemFilters
     ],
     testGroup "Normalization" [
-        testProperty "Normalized variant has the same specificity" normSpec,
-        testProperty "Normalization is idempotent" normIdem
+        testProperty "Normalized variant has the same specificity" normSpec
+      , testProperty "Normalization is idempotent" normIdem
     ],
     testGroup "Build an expression or pattern" [
-        testProperty "Check build of pattern 1" buildPattern1,
-        testProperty "Check build of pattern 2" buildPattern2
+        testProperty "Check build of pattern 1" buildPattern1
+      , testProperty "Check build of pattern 2" buildPattern2
     ],
     testGroup "Convert to binary and back" [
-        testProperty "Binary identity: selector group" (binaryEquivalent @SelectorGroup),
-        testProperty "Binary identity: selector" (binaryEquivalent @Selector),
-        testProperty "Binary identity: selector sequence" (binaryEquivalent @SelectorSequence),
-        testProperty "Binary identity: selector filter" (binaryEquivalent @SelectorFilter),
-        testProperty "Binary identity: namespace" (binaryEquivalent @Namespace),
-        testProperty "Binary identity: element name" (binaryEquivalent @ElementName),
-        testProperty "Binary identity: type selector" (binaryEquivalent @TypeSelector),
-        testProperty "Binary identity: attribute" (binaryEquivalent @Attrib),
-        testProperty "Binary identity: attribute name" (binaryEquivalent @AttributeName),
-        testProperty "Binary identity: class" (binaryEquivalent @Class),
-        testProperty "Binary identity: hash" (binaryEquivalent @Hash),
-        testProperty "Binary identity: pseudo class" (binaryEquivalent @PseudoClass),
-        testProperty "Binary identity: pseudo element" (binaryEquivalent @PseudoElement),
-        testProperty "Binary identity: pseudo selector sequence" (binaryEquivalent @PseudoSelectorSequence),
-        testProperty "Binary identity: nth" (binaryEquivalent @Nth)
+        testProperty "Binary identity: selector group" (binaryEquivalent @SelectorGroup)
+      , testProperty "Binary identity: selector" (binaryEquivalent @Selector)
+      , testProperty "Binary identity: selector sequence" (binaryEquivalent @SelectorSequence)
+      , testProperty "Binary identity: selector filter" (binaryEquivalent @SelectorFilter)
+      , testProperty "Binary identity: namespace" (binaryEquivalent @Namespace)
+      , testProperty "Binary identity: element name" (binaryEquivalent @ElementName)
+      , testProperty "Binary identity: type selector" (binaryEquivalent @TypeSelector)
+      , testProperty "Binary identity: attribute" (binaryEquivalent @Attrib)
+      , testProperty "Binary identity: attribute name" (binaryEquivalent @AttributeName)
+      , testProperty "Binary identity: class" (binaryEquivalent @Class)
+      , testProperty "Binary identity: hash" (binaryEquivalent @Hash)
+      , testProperty "Binary identity: pseudo class" (binaryEquivalent @PseudoClass)
+      , testProperty "Binary identity: pseudo element" (binaryEquivalent @PseudoElement)
+      , testProperty "Binary identity: pseudo selector sequence" (binaryEquivalent @PseudoSelectorSequence)
+      , testProperty "Binary identity: nth" (binaryEquivalent @Nth)
+      , testProperty "Binary identity: negation" (binaryEquivalent @Negation)
     ],
     testGroup "Check binary equality" [
-        testProperty "Binary uniqness: selector group" (uniqnessEncoding @SelectorGroup),
-        testProperty "Binary uniqness: selector" (uniqnessEncoding @Selector),
-        testProperty "Binary uniqness: selector sequence" (uniqnessEncoding @SelectorSequence),
-        testProperty "Binary uniqness: selector filter" (uniqnessEncoding @SelectorFilter),
-        testProperty "Binary uniqness: namespace" (uniqnessEncoding @Namespace),
-        testProperty "Binary uniqness: element name" (uniqnessEncoding @ElementName),
-        testProperty "Binary uniqness: type selector" (uniqnessEncoding @TypeSelector),
-        testProperty "Binary uniqness: attribute" (uniqnessEncoding @Attrib),
-        testProperty "Binary uniqness: attribute name" (uniqnessEncoding @AttributeName),
-        testProperty "Binary uniqness: class" (uniqnessEncoding @Class),
-        testProperty "Binary uniqness: hash" (uniqnessEncoding @Hash),
-        testProperty "Binary uniqness: pseudo class" (uniqnessEncoding @PseudoClass),
-        testProperty "Binary uniqness: pseudo element" (uniqnessEncoding @PseudoElement),
-        testProperty "Binary uniqness: pseudo selector sequence" (uniqnessEncoding @PseudoSelectorSequence),
-        testProperty "Binary uniqness: nth" (uniqnessEncoding @Nth)
+        testProperty "Binary uniqness: selector group" (uniqnessEncoding @SelectorGroup)
+      , testProperty "Binary uniqness: selector" (uniqnessEncoding @Selector)
+      , testProperty "Binary uniqness: selector sequence" (uniqnessEncoding @SelectorSequence)
+      , testProperty "Binary uniqness: selector filter" (uniqnessEncoding @SelectorFilter)
+      , testProperty "Binary uniqness: namespace" (uniqnessEncoding @Namespace)
+      , testProperty "Binary uniqness: element name" (uniqnessEncoding @ElementName)
+      , testProperty "Binary uniqness: type selector" (uniqnessEncoding @TypeSelector)
+      , testProperty "Binary uniqness: attribute" (uniqnessEncoding @Attrib)
+      , testProperty "Binary uniqness: attribute name" (uniqnessEncoding @AttributeName)
+      , testProperty "Binary uniqness: class" (uniqnessEncoding @Class)
+      , testProperty "Binary uniqness: hash" (uniqnessEncoding @Hash)
+      , testProperty "Binary uniqness: pseudo class" (uniqnessEncoding @PseudoClass)
+      , testProperty "Binary uniqness: pseudo element" (uniqnessEncoding @PseudoElement)
+      , testProperty "Binary uniqness: pseudo selector sequence" (uniqnessEncoding @PseudoSelectorSequence)
+      , testProperty "Binary uniqness: nth" (uniqnessEncoding @Nth)
+      , testProperty "Binary uniqness: negation" (uniqnessEncoding @Negation)
     ],
     testGroup "Check hash constraint for the Hashable instances" [
-        testProperty "Different hash implies different items: selector group" (hashingDifferent @SelectorGroup),
-        testProperty "Different hash implies different items: selector" (hashingDifferent @Selector),
-        testProperty "Different hash implies different items: selector sequence" (hashingDifferent @SelectorSequence),
-        testProperty "Different hash implies different items: selector filter" (hashingDifferent @SelectorFilter),
-        testProperty "Different hash implies different items: namespace" (hashingDifferent @Namespace),
-        testProperty "Different hash implies different items: element name" (hashingDifferent @ElementName),
-        testProperty "Different hash implies different items: type selector" (hashingDifferent @TypeSelector),
-        testProperty "Different hash implies different items: attribute" (hashingDifferent @Attrib),
-        testProperty "Different hash implies different items: attribute name" (hashingDifferent @AttributeName),
-        testProperty "Different hash implies different items: class" (hashingDifferent @Class),
-        testProperty "Different hash implies different items: hash" (hashingDifferent @Hash),
-        testProperty "Different hash implies different items: pseudo class" (hashingDifferent @PseudoClass),
-        testProperty "Different hash implies different items: pseudo element" (hashingDifferent @PseudoElement),
-        testProperty "Different hash implies different items: pseudo selector sequence" (hashingDifferent @PseudoSelectorSequence),
-        testProperty "Different hash implies different items: nth" (hashingDifferent @Nth)
+        testProperty "Different hash implies different items: selector group" (hashingDifferent @SelectorGroup)
+      , testProperty "Different hash implies different items: selector" (hashingDifferent @Selector)
+      , testProperty "Different hash implies different items: selector sequence" (hashingDifferent @SelectorSequence)
+      , testProperty "Different hash implies different items: selector filter" (hashingDifferent @SelectorFilter)
+      , testProperty "Different hash implies different items: namespace" (hashingDifferent @Namespace)
+      , testProperty "Different hash implies different items: element name" (hashingDifferent @ElementName)
+      , testProperty "Different hash implies different items: type selector" (hashingDifferent @TypeSelector)
+      , testProperty "Different hash implies different items: attribute" (hashingDifferent @Attrib)
+      , testProperty "Different hash implies different items: attribute name" (hashingDifferent @AttributeName)
+      , testProperty "Different hash implies different items: class" (hashingDifferent @Class)
+      , testProperty "Different hash implies different items: hash" (hashingDifferent @Hash)
+      , testProperty "Different hash implies different items: pseudo class" (hashingDifferent @PseudoClass)
+      , testProperty "Different hash implies different items: pseudo element" (hashingDifferent @PseudoElement)
+      , testProperty "Different hash implies different items: pseudo selector sequence" (hashingDifferent @PseudoSelectorSequence)
+      , testProperty "Different hash implies different items: nth" (hashingDifferent @Nth)
+      , testProperty "Different hash implies different items: negation" (hashingDifferent @Negation)
     ],
     testGroup "Nths" [
         testProperty "Produces only (strictly) positive values" positiveNth
